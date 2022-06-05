bd699137b05eb6696f97bd5cadf49d25edb939fd
franciscoamoes <franciscoamoes1@gmail.com>
2022-06-05 15:15:06 +0100

    api test

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
 	app.models
-	.predict(Clarifai.process.env.API_CLARIFAI, req.body.input)
+	.predict('process.env.API_CLARIFAI', req.body.input)
 	.then(data => {
 		res.json(data);
 	})
